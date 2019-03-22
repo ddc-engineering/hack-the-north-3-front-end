2543d0d55d64958e213e170631d4a5b1c175bb87
Joe Stringer <joe.stringer@infinityworks.com>
2019-03-22 23:48:31 +0000

    Devtools fix

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,14 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from "../reducers/rootReducer";
 import ReduxThunk from "redux-thunk";
 
 const configureStore = () => {
   return createStore(
     rootReducer,
-    compose(
+    composeWithDevTools(
       applyMiddleware(ReduxThunk),
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__() || compose
     )
   );
 };
